0dfc807832d10801b919ca1ce3a3ee83bdb563db
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-07-27 13:40:38 -0700

    add default settings to money format

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,5 +1,8 @@
 MoneyRails.configure do |config|
 
+  Money.default_currency = Money::Currency.new("CAD")
   config.no_cents_if_whole = false
+  Money.rounding_mode = BigDecimal::ROUND_HALF_EVEN
+  Money.locale_backend = :i18n
 
 end
